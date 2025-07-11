ee87ba058ab1127b9352f95345d5a22669cdecb0
islame <you@example.com>
2025-07-11 11:07:03 +0100

    change base to './'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/MDN-tutorial/",
+  base: "./",
   build: {
     outDir: "dist", // Ensure this matches your build directory
   },
