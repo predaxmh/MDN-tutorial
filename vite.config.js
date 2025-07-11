c1228b2028ce23cf772891d7d888b39828e15f2f
islame <you@example.com>
2025-07-11 09:42:47 +0100

    fixing problems

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/MDN-tutorial/",
+  build: {
+    outDir: "dist", // Ensure this matches your build directory
+  },
 });
